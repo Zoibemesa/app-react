99e29d4c69594910392d6d7489908603db838c73
Zoibemesa <20241831@itla.edu.do>
2026-06-05 04:47:58 +0000

    Agregando configuracion de Capacitor y actualizaciones

---
+++ android/app/src/main/java/com/zoibemesa/appreact/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zoibemesa.appreact;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zoibemesa.appreact',
+  appName: 'App React',
+  webDir: 'build'
+};
+
+export default config;
